8cbebb146851620a88e778d052e2b2fad15ed925
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-13 06:20:33 -0700

    Simple interest calculation script.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 #script for simple interest calculation.
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
-print("The simple interest is:", I)
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print("The simple interest is:", interest)
