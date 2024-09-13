825a80ec8357c6482114f07986001eb070e53841
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-13 05:56:49 -0700

    Operations script added.

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,3 +1,6 @@
 number1 = 10
 number2 = 5
+#number1 + number2 = 15
+#number1 - number2 = 5
+#number1 * number2 = 50
 print("Addition of 10 and 5 is 15 \nSubtraction of 10 and 5 is 5 \nMultiplication of 10 and 5 is 50")
