4fd8041a37edb39c6699de435f0af674d112f87f
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-22 05:44:51 -0700

    Loop for multipliation table.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 #for loop multiplication table
 number = int(input("Enter a number to see its multiplication table: ")
 
-for x in range(1, 10):
+for x in range(1, 11):
   z = number * x
   print(f"{number} * {x} = {z}", end = "\t")
