66ecbe5586d8075115b3a23e652101959ad290f1
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-22 13:02:25 -0700

    Daily reminder script.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,15 +1,15 @@
 #Daily task reminded script
 
 task = input("Enter your task: ")
-priority = ("Priority (high/medium/low): ")
-time = ("Is it time-bound? (yes/no): ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 for i in ["high", "medium", "low"]:
     match priority:
         case "i"
-        if time == "yes":
+        if time_bound == "yes":
             print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
-        elif time == "no":
+        elif time_bound == "no":
             print(f"Note: {task} needs your attention but you can do it when you have time.")
         else:
             print("Invalid input.")
