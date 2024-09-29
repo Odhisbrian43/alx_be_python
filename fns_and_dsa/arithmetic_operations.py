291c5278128653efbf363227c94365257d2b780e
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-29 02:30:15 -0700

    simple arithmetic operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,49 @@
+#function for arithmetics
+
+def perform_operation():
+
+    num1 = float(input("Enter first number: "))
+
+    num2 = float(input("Enter second number: "))
+
+    operation = input("Enter the operation (add, subtract, multiply, divide): ")
+
+    match operation:
+
+        case "add":
+
+            num1 + num2 = result
+
+            print("Result: ", result)
+
+        case "subtract":
+
+            num1 - num2 = result
+
+            print("Result: ", result)
+
+        case "multiply":
+
+            num1 * num2 = result
+
+            print("Result: " result)
+
+        case "divide":
+            
+            if num2 > 0:
+
+                num1 / num2 = result
+
+                print("Result: ", result)
+
+            else:
+
+                print("Cannot divide by zero")
+        case _:
+
+            print("Invalid operation entered, try again.")
+
+perform_operation()
+
+
+
