4f7674c899ccc00ef916328a18797dcec213e997
odhisbrian43 <odhisbrian43@gmail.com>
2024-09-29 23:12:23 +0300

    Date time updated.

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,31 @@
+#date time test
+
+from datetime import datetime
+
+from datetime import date
+
+from datetime import timedelta
+
+def display_current_datetime():
+
+    current_date = datetime.now()
+    
+    time = current_date.strftime("%d/%m/%Y %H:%M:%S")
+
+    print("Current date and time is ",time)
+
+display_current_datetime()
+
+def calculate_future_date():
+
+    current_date = datetime.now()
+
+    days_no = int(input("Enter the number of days to add to the current date: "))
+               
+    delta = timedelta(days = days_no)
+
+    future_date = current_date + delta
+
+    print(future_date)
+
+calculate_future_date()
