50bfe3c92d9274ee58341a250b6e36446920c174
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-05 01:48:54 -0700

    update calculator.

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+#Division calculator
+
+def safe_divide(numerator, denominator):
+    try:
+        if denominator == 0:
+           raise ZeroDivisionError ("Error: Cannot divide by zero.")
+        else:
+           return numerator / denominator
+    except:
+        str(numerator, denominator)
+        raise ValueError ("Error: Please enter numeric values only.")
+
