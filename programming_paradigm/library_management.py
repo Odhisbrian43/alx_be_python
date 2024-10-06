8e74d99f18d5a8ba8adc7ce24b2c086a2eb523ac
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-06 09:18:17 -0700

    update library.

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,27 @@
+#library
+
+class book:
+
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self.__is_checked_out = _is_checked_out
+    def book_id(self):
+        self.book_id = f"{self.tile} by {self.author} {_is_checked_out}"
+        return self.book_id
+class library:
+    __books = []
+
+    def add_book(self, title, author):
+        self.title = input()
+        self.author = input()
+        __books.appand("{self.title}, {self.author}") 
+    def check_out_book(self, title):
+        if self.title in __books:
+            pass
+    def return_book(self, title):
+        if self.title in __books:
+            False
+    def list_available_books(self):
+        for i in __books:
+            print(i)
