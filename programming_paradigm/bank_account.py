8a0f888a513eb8b864a65ef7854edde65883fa36
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-06 09:52:12 -0700

    update bank operations.

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+#Banking operations
+
+class BankAccount:
+    def __init__(self, account_balance):
+
+
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.amount = amount
+        self.amount = input()
+        self.account_balance == 0
+        return self.account_balance += self.amount
+
+    def withdraw(self, amount):
+        if self.account_balance > self.amount:
+            return self.account_balance - self.amount
+    def display_balance(self):
+        print(self.account_balance)
