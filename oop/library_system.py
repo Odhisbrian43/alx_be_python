589aecd31c8d3bf358a0c25e174d8501440ba159
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-13 10:17:22 -0700

    oop assignment

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,14 +4,17 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         self.file_size = file_size
+    def __str__(self):
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         self.page_count = page_count
+    def __str__(self):
 
 class Library:
    # def __init__(self, books):
