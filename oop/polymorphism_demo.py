258facebfb3c9cd455157641bf5e7f67ad6ea104
odhisbrian43 <odhisbrian43@gmail.com>
2024-10-13 10:05:00 -0700

    oop assignment

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -17,7 +17,7 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return math.pi * self.radius * self.radius
+        return math.pi * self.radius ** 2
 
 #if __name__ == "__main__":
  #   main()
